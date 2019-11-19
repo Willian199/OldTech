14566ea147f0caae8ab0130f59ecc0038cddef43
lucastumeleiro <lucasftumeleiro@gmail.com>
2019-11-18 21:13:02 -0300

    Tela principal

--- delphi/home.pas
+++ delphi/home.pas
@@ -4,10 +4,27 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.ExtCtrls;
 
 type
   TPrincipal = class(TForm)
+    MainMenu1: TMainMenu;
+    Cadastro: TMenuItem;
+    Item: TMenuItem;
+    Pessoa: TMenuItem;
+    Peca: TMenuItem;
+    Colecao: TMenuItem;
+    tipoMaterial: TMenuItem;
+    Fabricante: TMenuItem;
+    cadastroPessoa: TMenuItem;
+    Usuario: TMenuItem;
+    Controladoria: TMenuItem;
+    Museu: TMenuItem;
+    Setor: TMenuItem;
+    Relatorio: TMenuItem;
+    Sobre: TMenuItem;
+    Sair: TMenuItem;
+    Panel1: TPanel;
   private
     { Private declarations }
   public
