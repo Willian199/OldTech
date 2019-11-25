f53c406006c76f562d17008918509bc902e7e108
Luiz Fernando Coppini de Lima <310725@unoesc.lan>
2019-11-25 20:30:35 -0300

    Criado tela de usuario

--- delphi/museu.dpr
+++ delphi/museu.dpr
@@ -5,7 +5,8 @@ uses
   login in 'login.pas' {Form1},
   datamodulo in 'datamodulo.pas' {DataModule1: TDataModule},
   home in 'home.pas' {Principal},
-  ListaItem in 'ListaItem.pas' {Item};
+  ListaItem in 'ListaItem.pas' {Item},
+  Usuario in 'Usuario.pas' {Form2};
 
 {$R *.res}
 
@@ -15,6 +16,7 @@ begin
   Application.CreateForm(TForm1, Form1);
   Application.CreateForm(TDataModule1, DataModule1);
   Application.CreateForm(TPrincipal, Principal);
-  Application.CreateForm(TItem, CadastroItem);
+  Application.CreateForm(TCadastroItem, CadastroItem);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
