a941f503c7835f79826d13434931046eab532e40
Willian <willian.marchesan@gmail.com>
2019-11-29 20:12:43 -0300

    Doc: Criado indices

---
+++ banco/indices.sql
@@ -0,0 +1,9 @@
+CREATE INDEX pessoa_tipo_index ON pessoa (cd_tipopessoa);
+
+CREATE INDEX item_material_index ON item (cd_material);
+
+CREATE INDEX item_fabricante_index ON item (cd_fabricante);
+
+CREATE INDEX item_pais_index ON item (cd_pais);
+
+CREATE INDEX item_colecao_index ON item_colecao (cd_colecao);
