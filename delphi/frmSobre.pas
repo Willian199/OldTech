8d0d0bdc8d699f52f9356f369b4f59e0cada90af
Gustavovincenzi <artelaje.gustavo@gmail.com>
2019-12-02 19:20:22 -0300

    ajuste tela sobre

--- delphi/frmSobre.pas
+++ delphi/frmSobre.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.pngimage;
 
 type
   TnSobre = class(TForm)
